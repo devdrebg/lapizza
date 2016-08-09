a72202ae9a02222a7f1a7b2d06ac8e2491c17805
André Romário <andreromario@live.com>
2016-08-08 22:37:42 -0300

    Ending the first part of home page

--- site/elements/header.php
+++ site/elements/header.php
@@ -12,10 +12,38 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-md-2">
-						<h1><a href="" title=""><img src="../img/logo.png" alt="" class="img-responsive center-block"></a></h1>
+						<h1 class="logo"><a href="" title=""><img src="../img/logo.png" alt="" class="img-responsive center-block"></a></h1>
 					</div>
-					<div class="col-md-8">
-						a
+					<div class="col-md-3 col-md-offset-3">
+						<div class="navbar-header"> 
+							<button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#menu-header">
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+						</div>
+
+						<ul id="menu-header" class="nav navbar-nav collapse navbar-collapse bs-navbar-collapse">
+							<li><a href="#">Home</a></li>
+							<li>
+								<a href="#" id="menu-products" data-toggle="dropdown">Produtos <span class="caret"></span></a>
+
+								<ul class="dropdown-menu" role="menu" aria-labelledby="menu-products">
+									<li><a href="#">Categoria A</a></li>
+									<li><a href="#">Categoria B</a></li>
+									<li><a href="#">Categoria C</a></li>
+									<li><a href="#">Categoria D</a></li>
+									<li><a href="#">Categoria E</a></li>
+								</ul>		
+							</li>
+							<li><a href="#">Contato</a></li>
+					    </ul>
+					</div>
+					<div class="col-md-4">
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="#"><span class="glyphicon glyphicon-user"></span> Cadastre-se</a></li>
+							<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+					    </ul>
 					</div>
 				</div>
 			</div>
