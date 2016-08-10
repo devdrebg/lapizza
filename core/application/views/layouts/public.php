713d9607bb30f2f64037fefc4475e74d6e4746ba
andreromario <andreromario@live.com>
2016-08-10 15:52:15 -0400

    Template lib

---
+++ core/application/views/layouts/public.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    
+  </head>
+  <body>
+      <div id="contents"><?php echo $contents; ?></div>
+      <div id="footer">Copyright 2008</div>
+  </body>
+</html>
