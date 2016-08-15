77ced767c1e68dc0152eed9ce12e8bf806620a48
andreromario <andreromario@live.com>
2016-08-15 12:03:56 -0400

    Add meta viewport

--- site/application/views/layouts/user.php
+++ site/application/views/layouts/user.php
@@ -2,6 +2,7 @@
 <html lang="pt-BR">
 	<head>
 		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title></title>
 		<link rel="stylesheet" href="<?php echo base_url() . 'css/bootstrap.min.css'; ?>">
 		<link rel="stylesheet" href="<?php echo base_url() . 'fonts/font-awesome/css/font-awesome.min.css'; ?>">
