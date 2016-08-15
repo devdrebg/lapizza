d1c36cebcba93403f9ae9558002ad2a449462ead
andreromario <andreromario@live.com>
2016-08-15 12:06:02 -0400

    Add meta color bar on browser

--- site/application/views/layouts/public.php
+++ site/application/views/layouts/public.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="theme-color" value="#BC2026">
 		<title></title>
 		<link rel="stylesheet" href="<?php echo base_url() . 'css/bootstrap.min.css'; ?>">
 		<link rel="stylesheet" href="<?php echo base_url() . 'fonts/font-awesome/css/font-awesome.min.css'; ?>">
